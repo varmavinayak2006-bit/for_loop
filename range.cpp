0966ad2a18817ff5cece8f62dd36c7c42d5a7d8b
Vinayak2006 <varmavinayak2006@gmail.com>
2026-05-29 21:47:10 +0530

    Special range series

---
+++ range.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    long low, high , i , j , prod ;
+    cout<<"Enter high and low respectively ." ;
+    cin>>high>>low ;
+    j = high - low + 1 ;          // you will get product from range low to high 
+    for (i = 1; i <= j ; i++)    // low * i that is 1 then i++ to high * j 
+    {
+        prod = low * i ;
+        cout<<prod<<" ";
+        low ++ ;
+        
+    }
+    return 0 ;
+
+}
