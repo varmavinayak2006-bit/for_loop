4dc62689dd6365db46144e5127ba7ee4907c9bfa
Vinayak2006 <varmavinayak2006@gmail.com>
2026-05-31 20:40:24 +0530

    Hollow Rectangle

---
+++ rect.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+    int m , n , i , j ;
+    cin>>m;
+    cin>>n;
+    for (i = 1 ; i<=n;i++ ){
+        cout<<"*";
+    }
+    cout<<endl;
+    for (i = 1 ;i <= m - 2 ;i ++){
+        for (j = 1 ; j <=n ; j++){
+            if (j == 1 || j == n){
+                cout<<"*";
+            }
+            else{
+            cout<<" ";}
+        }
+        cout<<endl;
+    }
+    for (i = 1 ; i<=n;i++ ){
+        cout<<"*";
+    }
+	// Your code here
+    return 0;
+}
